da564519f4446a4d72dc28811151d40991bdd4ee
Brandon Kitt <brandon.kitt@hotmail.com>
2025-01-29 23:45:36 +0800

    feat(player-tile): update player tile (#19)
    Co-authored-by: Brandon Kitt <brandon.kitt@kittisolutions.com.au>

--- features/game/presentation/GameScreenStyle.tsx
+++ features/game/presentation/GameScreenStyle.tsx
@@ -1,51 +1,51 @@
-
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "#f5f5f5",
-      padding: 20,
-    },
-    header: {
-      fontSize: 24,
-      fontWeight: "bold",
-      marginBottom: 20,
-    },
-    scoreboard: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      marginBottom: 30,
-      width: "100%",
-    },
-    playerSection: {
-      alignItems: "center",
-      flex: 1,
-      marginHorizontal: 10,
-      padding: 20,
-      backgroundColor: "#e0e0e0",
-      borderRadius: 10,
-    },
-    playerName: {
-      fontSize: 18,
-      fontWeight: "600",
-      marginBottom: 10,
-    },
-    score: {
-      fontSize: 36,
-      fontWeight: "bold",
-      marginBottom: 10,
-    },
-    games: {
-      fontSize: 36,
-      fontWeight: "bold",
-      marginBottom: 20,
-    },
-    gameOrMatchBall: {
-      fontSize: 16,
-      fontWeight: "bold",
-    },
-  });
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+    padding: 20,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  scoreboard: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 30,
+    width: "100%",
+  },
+  playerSection: {
+    alignItems: "center",
+    minHeight: "auto",
+    flex: 1,
+    marginHorizontal: 10,
+    padding: 20,
+    backgroundColor: "#e0e0e0",
+    borderRadius: 10,
+  },
+  playerName: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginBottom: 10,
+  },
+  score: {
+    fontSize: 36,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  games: {
+    fontSize: 36,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  gameOrMatchBall: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});

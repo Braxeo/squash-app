9146c681c716056b6adaf89cdfd99eb36f637734
Brandon Kitt <brandon.kitt@hotmail.com>
2025-01-29 23:21:11 +0800

    feat(alert): update to remove alerts (#18)
    Co-authored-by: Brandon Kitt <brandon.kitt@kittisolutions.com.au>

--- features/rules/MatchRules.ts
+++ features/rules/MatchRules.ts
@@ -1,59 +1,61 @@
-import { GameConfigurationError } from "../errors/GameConfigurationError"
-import { PointsBy } from "./constants/Enums"
-import { WinningRequirement } from "./constants/Enums"
+import { GameConfigurationError } from "../errors/GameConfigurationError";
+import { PointsBy } from "./constants/Enums";
+import { WinningRequirement } from "./constants/Enums";
 
 export class MatchRules {
-    private gamesPerMatch: number
-    private pointsPerGame: number
-    private pointsBy: PointsBy
-    private winningRequirement: WinningRequirement
-    private warmupMinutes: number
+  private gamesPerMatch: number;
+  private pointsPerGame: number;
+  private pointsBy: PointsBy;
+  private winningRequirement: WinningRequirement;
+  public warmupMinutes: number;
 
-    constructor(
-        gamesPerMatch: number,
-        pointsPerGame: number,
-        pointsBy: PointsBy,
-        winningRequirement: WinningRequirement,
-        warmupMinutes: number
-    ) {
-        if(warmupMinutes <= 0) {
-            throw new GameConfigurationError("Warmup minutes must be > 0")
-        }
-
-        if(gamesPerMatch % 2 !== 1) {
-            throw new GameConfigurationError("Games in a match should be odd numbered")
-        }
-
-        this.gamesPerMatch = gamesPerMatch
-        this.pointsPerGame = pointsPerGame
-        this.pointsBy = pointsBy
-        this.winningRequirement = winningRequirement
-        this.warmupMinutes = warmupMinutes
+  constructor(
+    gamesPerMatch: number,
+    pointsPerGame: number,
+    pointsBy: PointsBy,
+    winningRequirement: WinningRequirement,
+    warmupMinutes: number
+  ) {
+    if (warmupMinutes <= 0) {
+      throw new GameConfigurationError("Warmup minutes must be > 0");
     }
 
-    public getGamesPerMatch(): number {
-        return this.gamesPerMatch
+    if (gamesPerMatch % 2 !== 1) {
+      throw new GameConfigurationError(
+        "Games in a match should be odd numbered"
+      );
     }
 
-    public getPointsPerGame(): number {
-        return this.pointsPerGame
-    }
+    this.gamesPerMatch = gamesPerMatch;
+    this.pointsPerGame = pointsPerGame;
+    this.pointsBy = pointsBy;
+    this.winningRequirement = winningRequirement;
+    this.warmupMinutes = warmupMinutes;
+  }
 
-    public getPointsBy(): PointsBy {
-        return this.pointsBy
-    }
+  public getGamesPerMatch(): number {
+    return this.gamesPerMatch;
+  }
 
-    public getWinningRequirement(): WinningRequirement {
-        return this.winningRequirement
-    }
+  public getPointsPerGame(): number {
+    return this.pointsPerGame;
+  }
 
-    public getWarmupMinutes(): number {
-        return this.warmupMinutes
-    }
+  public getPointsBy(): PointsBy {
+    return this.pointsBy;
+  }
 
-    // Method to return a description of the class instance
-    public describe(): string {
-        return `
+  public getWinningRequirement(): WinningRequirement {
+    return this.winningRequirement;
+  }
+
+  public getWarmupMinutes(): number {
+    return this.warmupMinutes;
+  }
+
+  // Method to return a description of the class instance
+  public describe(): string {
+    return `
         Match Configuration:
         - Games Per Match: ${this.gamesPerMatch}
         - Points Per Game: ${this.pointsPerGame}
@@ -61,5 +63,5 @@ export class MatchRules {
         - Points By: ${this.pointsBy}
         - Winning Requirements: ${this.winningRequirement}
         `;
-    }
+  }
 }
